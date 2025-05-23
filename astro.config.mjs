5f4b91e4950e2486d18ae5dd2b1aad7cf825b7c6
Fernanda Gonzalez <Fernanda.Gonzalez@axolotlsolutions.com>
2025-05-23 11:04:02 -0600

    New changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,12 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Keikooh.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['es', 'en'],
+  //   routing: {
+  //     prefixDefaultLocale: true,
+  //   }
+  // }
 });
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,13 +3,14 @@ export const languages = {
     es: 'Español',
 };
 
-export const defaultLang = 'es';
+export let defaultLang = 'es';
 
 export const ui = {
     en: {
         'title': 'Fer Gonzalez | Welcome to my portfolio',
         'btn.code': 'Code',
         'btn.preview': 'Preview',
+        'nav.title': 'My Port<span class="text-lavanda">folio</span>',
         'nav.home': 'Home',
         'nav.tech': 'Tech',
         'nav.education': 'Education',
@@ -23,13 +24,19 @@ export const ui = {
         'section.projects': 'Projects',
         'education.type1': "Bachelor's degree in",
         'education.area1': 'Computer Systems Engineering',
+        'education.time1': 'August 2020 - December 2024',
         'education.description1': 'Object oriented, structured and functional programming, design pattern, code optimization.',
         'education.type2': "Online Courses:",
         'education.description2': 'JavaScript, React, Node, TypeScript and Angular',
-        'experience.position': 'Software Engineering Intern',
-        'experience.time': 'Aug 2024 - April 2025',
-        'experience.location': 'Leon, Gto. Mexico',
-        'experience.description': [
+        'experience.position2': 'Software Engineering Intern',
+        'experience.time1': 'May 2024 - August 2024',
+        'experience.time2': 'August 2024 - April 2025',
+        'experience.location1': 'Leon Gto. Mexico',
+        'experience.location2': 'Leon, Gto. Mexico',
+        'experience.description1': [
+            'I developed a mobile tracking app, including interface design, service implementation, Google Maps API integration, database creation, deployment and testing.',
+        ],
+        'experience.description2': [
                                     'Collaborated on the front-end development of a claim tracking application, creating a tracking section, customized dynamic charts, and performing general maintenance.',
                                     'Implemented and maintained REST services based on functional requirements, creating endpoints, designing entities, integrating them with existing gateways, and ensuring proper functionality.',
                                     'Developed a new workflow to optimize the receipt settlement process for insurance policies in a treasury application, reducing processing time by 70%.'
@@ -42,6 +49,7 @@ export const ui = {
         'title': 'Fer Gonzalez | Bienvenido a mi portafolio',
         'btn.code': 'Código',
         'btn.preview': 'Vista previa',
+        'nav.title': 'Mi Porta<span class="text-lavanda">folio</span>',
         'nav.home':'Inicio',
         'nav.tech':'Tecnología',
         'nav.education':'Educación',
@@ -55,13 +63,20 @@ export const ui = {
         'section.projects': 'Proyectos',
         'education.type1': "Licenciatura en",
         'education.area1': 'Ingeniería en Sistemas Computacionales',
+        'education.time1': 'Agosto 2020 - Diciembre 2024',
         'education.description1': 'Programación orientada a objetos, estructurada y funcional, patrones de diseño, optimización de código.',
         'education.type2': "Cursos online:",
         'education.description2': 'JavaScript, React, Node, TypeScript y Angular',
-        'experience.position': 'Becario en Ingeniería de Software',
-        'experience.time': 'Agosto 2024 - Abril 2025',
-        'experience.location': 'León Gto. México',
-        'experience.description': [
+        'experience.position1': 'Residencias profesionales',
+        'experience.position2': 'Desarrollador Junior',
+        'experience.time1': 'Mayo 2024 - Agosto 2024',
+        'experience.time2': 'Agosto 2024 - Abril 2025',
+        'experience.location1': 'León Gto. México',
+        'experience.location2': 'León Gto. México',
+        'experience.description1': [
+                                    'Desarrollé una aplicación móvil para seguimiento incluyendo el diseño de la interfaz, implementación de servicios, integración con la API de Google Maps, creación de la base de datos, despliegue y pruebas.',
+                                ],
+        'experience.description2': [
                                     'Colaboré en el desarrollo front-end de una aplicación para el seguimiento de siniestros, creando una sección de tracking, gráficas dinámicas personalizadas y haciendo mantenimiento general.',
                                     'Implementé y dí mantenimiento a servicios REST según requerimientos funcionales, creando endpoints, diseñando entidades, integrándolos a gateways existentes y asegurando su correcto funcionamiento.',
                                     'Desarrollé un nuevo flujo para optimizar el proceso de liquidación de recibos para pólizas en una aplicación de tesorería, logrando reducir el tiempo de procesamiento en un 70%.',
